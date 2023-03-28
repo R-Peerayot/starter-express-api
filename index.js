b75ffd8620944b8b40fc13dbeb4a2b0c530bb79b
R-Peerayot <129133919+R-Peerayot@users.noreply.github.com>
2023-03-28 14:29:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000)*/
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {5JsshShUjK9sZlYxwRkyV8roZvcgvNl96wlv2OQi4GxFO+MMMw7Ap84bZ+w9v5jHSbJRIA0QdFTO3wR0gKNrctFDQ0y5z5kC5aZPb6OMpPpYwwReFshxhamWOP+swldwwZBtfjHqeP9xzf2ahFySHAdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
